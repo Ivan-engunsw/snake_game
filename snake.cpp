51ed7f630501edcdb5af24a62ca728dfdb99c57a
Ivan <z5557064@ad.unsw.edu.au>
2025-01-03 11:49:22 +0200

    Added relevant comments to the code in snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -4,14 +4,17 @@
 using namespace std;
 
 int main() {
+    //width and height of the matrix
     int width;
     int height;
     cin >> width >> height;
 
+    //x and y coordinates of the apple
     int xApple;
     int yApple;
     cin >> xApple >> yApple;
 
+    //number of snakes
     int N;
     cin >> N;
 
@@ -22,9 +25,11 @@ int main() {
     matrix[yApple][xApple] = 5;
 
     while (number_snakes < N) {
+        //k is the number of pair of segments of the snake
         int k;
         cin >> k;
 
+        //populating the snake vector with the x and y coordinates of the points of interest of the snake
         int size = 2 * k + 1;
         vector<int> snake(size);
         snake[0] = k;
@@ -33,11 +38,14 @@ int main() {
             cin >> snake[i];
         }
 
+        //populating the matrix with the snake's body
         for (size_t j = 1; j < (snake.size() - 2); j += 2) {
             int x = snake[j];
             int l = j + 1;
             int y = snake[l];
             matrix[y][x] = (number_snakes + 1);
+
+            //populating the matrix with the snake's body using two consecutive coordinates given
             if (j < snake.size()) {
                 int nextX = snake[j + 2];
                 int nextL = j + 3;
@@ -64,9 +72,11 @@ int main() {
                 }
             }
         }
+
         number_snakes += 1;
     }
 
+    //printing the matrix
     for (auto row : matrix) {
         for (auto element : row) {
             cout << element << ' ';
